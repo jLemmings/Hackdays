22d2dde0f5a2360a87d4372ca5523581d88a9931
jLemmings <josh@hemmings.ch>
2021-04-28 10:48:01 +0200

    add vuetify framework

--- frontend/bikekingdom/src/App.vue
+++ frontend/bikekingdom/src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      align-center center
+    >
+        <v-img
+          alt="BikeKingdom Logo"
+          contain
+
+          src="https://www.bikekingdom.ch/microsites-assets/bike-kingdom/Logos/image-thumb__4113581__landingpage-logo/BikeKingdomLenzerheide_LOGO_WideBlack.webp"
+          transition="scale-transition"
+          height="50px"
+        />
+
+    </v-app-bar>
+
+    <v-main>
+      <Map/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Map,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
