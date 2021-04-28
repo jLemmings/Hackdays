d4f9e88ebdb52a45f51bc9d5c2e72d61c0c980c4
Anna Magdalena Huber <annamagdalenahuber@gmail.com>
2021-04-28 14:35:27 +0200

    Add Bergbahn data to db

--- backend/bulk_import.py
+++ backend/bulk_import.py
@@ -1,4 +1,13 @@
 import mysql.connector
+import ast
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point, LineString
+import os
+from sqlalchemy import create_engine, types
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 mydb = mysql.connector.connect(
     host="127.0.0.1",
@@ -11,3 +20,17 @@ mydb = mysql.connector.connect(
 print(mydb)
 
 cursor = mydb.cursor()
+
+def import_bergbahn_data(path):
+    # path = "C:/Users/annam/OneDrive/Documents/dev/MSC/Hackdays/data"
+    for file in os.listdir(path):
+        engine = create_engine('mysql://root:example@localhost:3307/Bikekingdom')
+        df = pd.read_excel(path+"/"+file)
+        df[['Datum', 'Wochentag']] = df['Tag'].str.split(" ", expand=True,)
+        file_name = file.split(".")[0].replace(" ","_")
+        df.to_sql(file_name, con=engine, if_exists='replace', index=False, index_label=None, chunksize=None,dtype=None, method=None)
+        print(f"Wrote {file_name} to DB")
+
+
+if __name__ == '__main__':
+    import_bergbahn_data("C:/Users/annam/OneDrive/Documents/dev/MSC/Hackdays/data")
