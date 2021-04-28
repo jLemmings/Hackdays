367b5f6608abbb90d9f654ebb719d346ee93cc3e
Anna Magdalena Huber <annamagdalenahuber@gmail.com>
2021-04-28 18:22:40 +0200

    Bergbahn get function

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -1,62 +1,62 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
 import pandas as pd
 import mysql.connector
-
+from sqlalchemy import create_engine, types
+import pymysql
+pymysql.install_as_MySQLdb()
 mydb = mysql.connector.connect(
     host="127.0.0.1",
     port="3307",
     user="root",
     passwd="example"
  )
-
+cursor = mydb.cursor()
 print(mydb)
 
 app = Flask(__name__)
 api = Api(app)
 
-class Trails(Resource):
+class Bergbahn(Resource):
     def get(self):
-        data = pd.read_csv('users.csv')
-        data = data.to_dict('records')
+        argument = request.args
+        print(argument)
+        name = request.args.get("name")
+        print(name)
+        stunde = request.args.get("stunde")
+        print(stunde)
+        wochentag = request.args.get("wochentag")
+        all_hours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+        string_all_hours = str(tuple([key for key in all_hours])).replace(',)', ')')
+        all_days = ["Mo","Di","Mi","Do","Fr","Sa","So"]
+        string_all_days = str(tuple([key for key in all_days])).replace(',)', ')')
+        if name == None:
+            raise ValueError
+            print("Name must be given")
+        if stunde != None:
+            stunde = [int(stunde)]
+            stunde = str(tuple([key for key in stunde])).replace(',)', ')')
+        else:
+            stunde = string_all_hours
+        if wochentag != None:
+            wochentag = [wochentag]
+            wochentag = str(tuple([key for key in wochentag])).replace(',)', ')')
+        else:
+            wochentag = string_all_days
+        name= name.replace("'", "")
+        print(name)
+        print(stunde)
+        print(wochentag)
+        engine = create_engine('mysql://root:example@localhost:3307/Bikekingdom')
+        sql_statement = f"SELECT * FROM Bikekingdom.{name} WHERE stunde IN  {stunde} and wochentag IN {wochentag}"
+        cursor.execute(sql_statement)
+        data = cursor.fetchall()
         return {'data' : data}, 200
 
 
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('name', required=True)
-        parser.add_argument('age', required=True)
-        parser.add_argument('city', required=True)
-        args = parser.parse_args()
-
-        data = pd.read_csv('users.csv')
-
-        new_data = pd.DataFrame({
-            'name'      : [args['name']],
-            'age'       : [args['age']],
-            'city'      : [args['city']]
-        })
-
-        data = data.append(new_data, ignore_index = True)
-        data.to_csv('users.csv', index=False)
-        return {'data' : new_data.to_dict('records')}, 201
-
-    def delete(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('name', required=True)
-        args = parser.parse_args()
-
-        data = pd.read_csv('users.csv')
-
-        data = data[data['name'] != args['name']]
-
-        data.to_csv('users.csv', index=False)
-        return {'message' : 'Record deleted successfully.'}, 200
-
-
-
 # Add URL endpoints
-api.add_resource(Trails, '/trails')
+api.add_resource(Bergbahn,'/bergbahn')
+
 
 if __name__ == '__main__':
     app.run()
